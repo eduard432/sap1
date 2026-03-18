0f0eae5f8356a9b00a7a4a228ccb40e145d830fe
eduard432 <juantokx@gmail.com>
2026-03-18 08:14:05 -0600

    Program counter upgrades

--- program_counter.vhd
+++ program_counter.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity program_counter is
     generic (
@@ -8,6 +9,10 @@ entity program_counter is
     port (
         clk: in std_logic;
         reset: in std_logic;
+        CE: in std_logic; -- Counter enable
+        CO: in std_logic; -- Counter out
+        load: in std_logic;
+        data_in: in std_logic_vector(bits - 1 downto 0);
         counter: out std_logic_vector(bits - 1 downto 0)
     );
 end entity;
@@ -20,9 +25,13 @@ begin
         if rising_edge(clk) then
             if reset = '1' then
                 cnt_temp <= (others => '0');
-            else
+            elsif load = '1' then
+                cnt_temp <= data_in;
+            elsif CE = '1' then
                 cnt_temp <= cnt_temp + 1;
             end if;
         end if;
     end process;
+
+    counter <= cnt_temp when CO '1' else (others => '0')
 end architecture bh;
