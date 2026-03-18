08fc8537f64b4c0266cd7b01fe2a940f75764eec
eduard432 <juantokx@gmail.com>
2026-03-18 17:39:39 -0600

    Add clock entity and architecture implementation

---
+++ clock.vhd
@@ -0,0 +1,37 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity clock is
+    generic (
+        pulse: integer := 10
+    );
+    port (
+        clk: in std_logic;          -- board cloack
+        manual_clk: in std_logic;   -- button clock
+        sel: in std_logic;          -- select between two modes
+        HLT: in std_logic;          -- Stop de computer
+        clk_out: out std_logic      -- Clock output
+    );
+end entity clock;
+
+architecture bh of clock is
+    signal new_clock: std_logic := '0';
+begin
+    process(clk)
+        variable count: integer := 0;
+    begin
+        if(rising_edge(clk)) then
+            count := count + 1;
+            if(count = pulse) then
+                new_clock <= not new_clock;
+                count := 0;
+            else 
+                new_clock <= '0';
+            end if;
+        end if;
+    end process;
+
+    clk_out <= (new_clock when sel = '1' else manual_clk)
+           when HLT = '0' else '0';
+
+end architecture bh;
