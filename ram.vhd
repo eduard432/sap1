e4ba7f8109761d9000c672d45587cfd8475ba0b8
eduard432 <juantokx@gmail.com>
2026-03-22 14:22:36 -0600

    Refactor RAM and SAP1 entity: standardize signal names, add missing ports, and enhance component definitions for clarity and functionality

--- ram.vhd
+++ ram.vhd
@@ -5,12 +5,13 @@ use ieee.numeric_std.all;
 entity ram is
     port (
         clk      : in  std_logic;
-        we       : in  std_logic;
+        WE       : in  std_logic;
+        OE       : in  std_logic;
         addr     : in  std_logic_vector(7 downto 0);
         data_in  : in  std_logic_vector(7 downto 0);
         data_out : out std_logic_vector(7 downto 0)
     );
-end entity;
+end entity ram;
 
 architecture bh of ram is
     type ram_type is array (0 to 255) of std_logic_vector(7 downto 0);
@@ -20,12 +21,14 @@ begin
     process(clk)
     begin
         if rising_edge(clk) then
-            if we = '1' then
+            if WE = '1' then
                 RAM(to_integer(unsigned(addr))) <= data_in;
             end if;
 
             data_out <= RAM(to_integer(unsigned(addr)));
         end if;
     end process;
+
+    data_out <= RAM(to_integer(unsigned(addr))) when OE = '1' else (others => 'Z');
 
 end architecture;
--- sap1.vhd
+++ sap1.vhd
@@ -3,21 +3,43 @@ use ieee.std_logic_1164.all;
 
 entity sap1 is
     port (
+        -- Board signals:
         clk: in std_logic;
         clk_out: out std_logic;
-        reset: in std_logic;
-        CE: in std_logic; -- Counter enable
-        CO: in std_logic; -- Counter out
-        load: in std_logic;
-        data_in: in std_logic_vector(3 downto 0);
-        counter: out std_logic_vector(3 downto 0)
+        seg1, seg2, seg3: out std_logic_vector(6 downto 0)
+        -- SAP signals in:
+        -- Program Counter (PC)
+        CO: in std_logic;       -- Program counter out
+        J: in std_logic;        -- Jump (program counter in)
+        CE: in std_logic;       -- Count enable (increment)
+        -- A Register (A)
+        AI: in std_logic;       -- Register in
+        AO: in std_logic;       -- Register out
+        -- ALU:
+        EO: in std_logic;       -- Sum out
+        SU: in std_logic;       -- Substract
+        CY: out std_logic;      -- Carry bit (used with JC)
+        -- B register (B)
+        BI: in std_logic;       -- Register in
+        BO: in std_logic;       -- Register out
+        -- OUT (display):
+        OI: in std_logic;       -- Output register in
+        -- MAR (Memory Address Register)
+        MI: in std_logic;       -- MAR in
+        -- RAM
+        RI: in std_logic;       -- RAM in
+        RO: in std_logic;       -- RAN out
+        -- IR (Instruction Register)
+        II: in std_logic;       -- Instruction Register in
+        IO: in std_logic;       -- Instruction Register out
+
     );
 end entity sap1;
 
 architecture bh of sap1 is
     component clock is
         generic (
-            pulse: integer := 25000000
+            pulse: integer := 25000000  -- 1Hz
         );
         port (
             clk: in std_logic;
@@ -29,7 +51,7 @@ architecture bh of sap1 is
     end component clock;
 
     signal global_clk: std_logic := '0';
-
+    signal main_bus: 
 
     component regs is
         generic (
@@ -60,6 +82,34 @@ architecture bh of sap1 is
         );
     end component program_counter;
 
+    component ram is
+        port (
+            clk      : in  std_logic;
+            we       : in  std_logic;
+            addr     : in  std_logic_vector(7 downto 0);
+            data_in  : in  std_logic_vector(7 downto 0);
+            data_out : out std_logic_vector(7 downto 0)
+        );
+    end component ram;
+
+    component output is
+        port (
+            bin: in std_logic(7 downto 0);
+            seg1, seg2, seg3: out std_logic_vector(6 downto 0)
+        );
+    end component output;
+
+    component alu is
+        generic (
+            bits := 8
+        );
+        port (
+            A, B    : in  std_logic_vector(bits - 1 downto 0);
+            op      : in  std_logic;
+            result  : out std_logic_vector(bits - 1 downto 0);
+            carry   : out std_logic
+        );
+    end component alu;
 
     signal temp_out_leds: std_logic_vector(7 downto 0);
 begin
@@ -91,18 +141,18 @@ begin
         counter => counter
     );
 
-    -- A_REGSITER: regs
-    -- generic map (
-    --     8
-    -- )
-    -- port map (
-    --     clk => global_clk,
-    --     clear => not clear,
-    --     WE => not WE,
-    --     OE => OE,
-    --     I => I,
-    --     Q => temp_out_leds
-    -- );
+    A_REGSITER: regs
+    generic map (
+        8
+    )
+    port map (
+        clk => global_clk,
+        clear => not clear,
+        WE => not WE,
+        OE => OE,
+        I => I,
+        Q => temp_out_leds
+    );
     
     -- out_leds <= temp_out_leds when sel_leds = '1' else I;
 end architecture bh;
